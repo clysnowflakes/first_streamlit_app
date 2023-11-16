5777710841af375f2908055df9482943061c575b
clysnowflakes <150875985+clysnowflakes@users.noreply.github.com>
2023-11-16 14:25:45 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
-
-
+import requests
+import snowflake.connector
+from urllib.error import URLError
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
