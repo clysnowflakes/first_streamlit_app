fdf36b028b1509d09bda6d94bfc843579abfb4b1
clysnowflakes <150875985+clysnowflakes@users.noreply.github.com>
2023-11-16 15:23:57 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,6 @@ streamlit.write('The user entered ', fruit_choice)
 
 #import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
-streamlit.header("Fruityvice Fruit Advice!")
 
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
